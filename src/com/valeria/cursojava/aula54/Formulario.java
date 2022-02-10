29980d682faa7c902b70cf802d72a35e842579e4
Valeria Garcia <valeriabeserragarcia@gmail.com>
2022-02-10 09:27:51 -0300

    Aula 54 - enumeradores como classe

---
+++ src/com/valeria/cursojava/aula54/Formulario.java
@@ -0,0 +1,17 @@
+package com.valeria.cursojava.aula54;
+
+public class Formulario {
+	
+	enum Genero {
+		FEMININO('F'), MASCULINO('M');
+		
+		private char valor;
+		
+		private Genero(char valor) {
+			this.valor = valor;
+		}
+	}
+	
+	private String nome;
+	private Genero genero;
+}
