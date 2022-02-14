7215e7d90bbc291b7016e029ca687eb287407674
Valeria Garcia <valeriabeserragarcia@gmail.com>
2022-02-14 10:06:42 -0300

    Aula 69 - Thread: interface Runnable

---
+++ src/com/valeria/cursojava/aula68/Teste.java
@@ -0,0 +1,15 @@
+package com.valeria.cursojava.aula68;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		MinhaThreadRunnable thread1 = new MinhaThreadRunnable("#1", 900);
+		
+		MinhaThreadRunnable thread2 = new MinhaThreadRunnable("#2", 650);
+		
+		MinhaThreadRunnable thread3 = new MinhaThreadRunnable("#3", 1100);
+
+	}
+
+}
