340f06e8901d835118e61637077fd0c0e7f4f104
Valeria Garcia <valeriabeserragarcia@gmail.com>
2022-02-15 09:49:16 -0300

    Aula 79 - String - fazendo buscas - indexOf, lastIndexOf, contains

---
+++ src/com/valeria/cursojava/aula75_84/Aula79.java
@@ -0,0 +1,27 @@
+package com.valeria.cursojava.aula75_84;
+
+public class Aula79 {
+
+	public static void main(String[] args) {
+
+
+		String banana = "banana";
+		String ana = "ana";
+		
+		System.out.println(banana.indexOf('x'));
+		System.out.println(banana.indexOf('b'));
+		System.out.println(banana.indexOf('a')); //retorna o indice do primeiro 'a' encontrado 
+		System.out.println(banana.indexOf('n'));
+		
+		System.out.println(banana.indexOf(ana));
+		
+		System.out.println(banana.lastIndexOf('a'));
+		System.out.println(banana.lastIndexOf(ana));
+		
+		System.out.println(banana.contains(ana));
+		System.out.println(banana.contains("ceu"));
+
+
+	}
+
+}
