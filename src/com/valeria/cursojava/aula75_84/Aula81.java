e5d428a645e46c7fb5d5337d1495735b81297434
Valeria Garcia <valeriabeserragarcia@gmail.com>
2022-02-15 10:18:28 -0300

    Aula 81 - String - maiuscula/minuscula- toLowerCase, toUpperCase

---
+++ src/com/valeria/cursojava/aula75_84/Aula81.java
@@ -0,0 +1,16 @@
+package com.valeria.cursojava.aula75_84;
+
+public class Aula81 {
+
+	public static void main(String[] args) {
+
+
+		String teste = "Teste";
+		
+		System.out.println(teste);
+		System.out.println(teste.toLowerCase());
+		System.out.println(teste.toUpperCase());
+
+	}
+
+}
